dba2d55448ff366e2c5883b8a71326700f364334
AokijiS <aokijisamp@gmail.com>
2025-10-15 21:20:58 +0200

    main

--- main.js
+++ main.js
@@ -229,3 +229,16 @@ submitBtn.addEventListener('click', ()=>{
 });
 
 
+
+document.querySelectorAll('.menu-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const info = btn.closest('li').querySelector('.more-info');
+    if (info) {
+      info.style.display = info.style.display === 'block' ? 'none' : 'block';
+    }
+  });
+});
+
+
+
+
